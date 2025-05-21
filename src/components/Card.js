2133103e5d7e71bcd0c388110c9df708ef6cd86b
ThaissaDomiciano <thaissadomiciano@icloud.com>
2025-05-21 08:59:36 -0300

    conectando criar turmas com o backend

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,13 @@
 import './Card.css';
 
-const Card = ({ titulo, professor, pendentes, entregues, onClick }) => {
+const Card = ({ titulo, professor, onClick }) => {
     return (
         <div className="card">
             <div className="card-content">
                 <h3 className="titulo-card">{titulo}</h3>
                 <p className="professor-label"><strong>PROFESSOR:</strong> {professor}</p>
                 <div className="linha-separadora"></div>
-                <div className="status">
-                    <span className='titulo-pendente'>PENDENTES: <span className="pendente">{pendentes}</span></span>
-                    <span className='titulo-entregue'>ENTREGUES: <span className="entregue">{entregues}</span></span>
-                </div>
+                
             </div>
             <div className="card-footer" onClick={onClick}>
                 SALA
--- src/pages/Professor/HomeProfessor.js
+++ src/pages/Professor/HomeProfessor.js
@@ -1,4 +1,5 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
+import { useNavigate } from "react-router-dom";
 import Card from "../../components/Card";
 import './HomeProfessor.css';
 import Header from '../../components/Header';
@@ -7,20 +8,7 @@ import axios from 'axios';
 
 const HomeProfessor = ({ usuario, onLogout }) => {
   const [turmas, setTurmas] = useState([]);
-
-  useEffect(() => {
-    axios.get(`http://localhost:8080/turmas/professor/${usuario.id}`)
-      .then(res => {
-        const turmasFormatadas = res.data.map(turma => ({
-          titulo: turma.nome,
-          professor: usuario.nome,
-          pendentes: 0, // Pode puxar de /tarefas depois
-          entregues: 0
-        }));
-        setTurmas(turmasFormatadas);
-      })
-      .catch(err => console.error("Erro ao buscar turmas do professor:", err));
-  }, [usuario]);
+  const [novaTurma, setNovaTurma] = useState('');
 
   const linksProfessor = [
     { to: '/homeProfessor', label: 'INÍCIO' },
@@ -28,14 +16,70 @@ const HomeProfessor = ({ usuario, onLogout }) => {
     { to: '/cadastro-atividade', label: 'CADASTRO' },
   ];
 
+  const buscarTurmas = useCallback(async () => {
+    try {
+      const res = await axios.get(`http://localhost:8080/turmas/professor/${usuario.id}`);
+      const turmasFormatadas = res.data.map(turma => ({
+        titulo: turma.nomeMateria,
+        professor: usuario.nome,
+        pendentes: 0,
+        entregues: 0
+      }));
+      setTurmas(turmasFormatadas);
+    } catch (err) {
+      console.error("Erro ao buscar turmas do professor:", err);
+    }
+  }, [usuario]);
+
+  const criarTurma = async () => {
+    if (!novaTurma.trim()) return;
+
+    try {
+      await axios.post('http://localhost:8080/turmas', {
+        nomeMateria: novaTurma,
+        professor: { id: usuario.id }
+      });
+      setNovaTurma('');
+      buscarTurmas();
+    } catch (error) {
+      console.error('Erro ao criar turma:', error);
+    }
+  };
+
+  useEffect(() => {
+    buscarTurmas();
+  }, [buscarTurmas]);
+
+  const navigate = useNavigate();
+
   return (
     <div className="container-professor">
       <Header links={linksProfessor} nomeUsuario={usuario.nome} onLogout={onLogout} />
       <img src={Banner} alt="Banner" className="banner" />
-      <h2 className="titulo-professor">TURMAS</h2>
+        <h3 className="titulo-professor">CRIAR NOVA TURMA</h3>
+      <div className="criar-turma-box">
+        <div className="criar-turma-form">
+          <label htmlFor="nomeTurma" className="label-turma">NOME DA TURMA:</label>
+          <input
+            id="nomeTurma"
+            type="text"
+            placeholder="INFORME O NOME DA NOVA TURMA"
+            className="input-criar-turma"
+            value={novaTurma}
+            onChange={(e) => setNovaTurma(e.target.value)}
+          />
+          <button className="btn-criar-turma" onClick={criarTurma}>
+            CRIAR
+          </button>
+        </div>
+      </div>
+
+      <h2 className="titulo-professor">TURMAS CRIADAS</h2>
       <div className="turmas-container">
         {turmas.map((turma, index) => (
-          <Card key={index} {...turma} />
+          <Card key={index} {...turma} 
+          onClick={() => navigate('/analise')}
+          />
         ))}
       </div>
     </div>
